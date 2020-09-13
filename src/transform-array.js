f96a00ba425241806f1219301ffd60aeaeaa5d70
Nick Petrenko <extendsnull@gmail.com>
2020-09-13 23:16:40 +0300

    feat: add "Transform array" solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,41 @@
-const CustomError = require("../extensions/custom-error");
+const sequences = [
+  '--discard-next',
+  '--discard-prev',
+  '--double-next',
+  '--double-prev',
+];
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+
+  return arr.reduce((accumulator, item, index) => {
+    if (sequences.includes(item)) {
+      return accumulator;
+    }
+
+    const prev = arr[index - 1];
+    const next = arr[index + 1];
+
+    if (prev === sequences[0]) {
+      return accumulator;
+    }
+
+    if (prev === sequences[2]) {
+      accumulator.push(item, item);
+    } else {
+      accumulator.push(item);
+    }
+
+    if (next === sequences[1]) {
+      accumulator.pop();
+    }
+
+    if (next === sequences[3]) {
+      accumulator.push(item);
+    }
+
+    return accumulator;
+  }, []);
 };
